16a3fcdd9bcb238dd8d0402d2b5738da32896a32
Greviansonula <greviansonula@gmail.com>
2023-01-13 00:26:49 +0300

    Update Training script

--- training.py
+++ training.py
@@ -1,12 +1,11 @@
 import pickle
 from sklearn.ensemble import RandomForestClassifier
 
-with open('x_train.pkl', 'rb') as file:
-    x_train = pickle.load(file)
+with open('data_splits.pkl', 'rb') as file:
+    data_splits = pickle.load(file)
 
-with open('y_train.pkl', 'rb') as file:
-    y_train = pickle.load(file)
+x_train, y_train = data_splits['x_train'], data_splits['y_train']
 
 classifier = RandomForestClassifier(n_estimators=100 ,criterion='entropy' , random_state=0).fit(x_train, y_train)
 with open('model.pkl', 'wb') as file:
-    pickle.dump(classifier)
+    pickle.dump(classifier, file)
