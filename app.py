8f9e73dd140e38310923abfd6f6906ad8abb6d50
Greviansonula <greviansonula@gmail.com>
2023-01-13 05:32:06 +0000

    Web application

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+from keras.models import load_model
+
+app = Flask(__name__)
+model = load_model('swahili.h5')
+
+
+def preprocess_text():
+  pass
+
+
+@app.route('/')
+def index():
+  return render_template('templates/index.html')
+
+
+@app.route('/classify', methods=['POST'])
+def classify():
+  text = request.form['text']
+  # preprocess the text
+  text = preprocess_text(text)
+  result = model.predict(text)
+  return render_template('result.html', result=result)
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
---
+++ preprocess_input.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+import re
+import nltk
+
+nltk.download('punkt')
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+
+
+train = pd.read_csv("dataset/Train.csv")
+
+# Associate Category names with numerical index and save it in new column CategoryId
+target_category = train['category'].unique()
+
+train['categoryId'] = train['category'].factorize()[0]
+
+# Create a new pandas dataframe "category", which only has unique Categories, also sorting this list in order of CategoryId values
+category = train[['category', 'categoryId']].drop_duplicates().sort_values('categoryId')
+
+def remove_tags(text):
+    remove = re.compile(r'')
+    return re.sub(remove, '', text)
+train['content'] = train['content'].apply(remove_tags)
+
+
+def special_char(text):
+    reviews = ''
+    for x in text:
+        if x.isalnum():
+            reviews = reviews + x
+        else:
+            reviews = reviews + ' '
+    return reviews
+train['content'] = train['content'].apply(special_char)
+
+
+def convert_lower(text):
+    return text.lower()
+train['content'] = train['content'].apply(convert_lower)
+
+
+x = train['content']
+y = train['categoryId']
+
+from sklearn.feature_extraction.text import CountVectorizer
+x = np.array(train.iloc[:,0].values)
+y = np.array(train.categoryId.values)
+cv = CountVectorizer(max_features = 5000)
+x = cv.fit_transform(train.content).toarray()
+
+
+
+
+
