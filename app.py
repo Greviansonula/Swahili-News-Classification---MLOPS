358c6fed44a4db63a55cc28094faa6c621406f19
Greviansonula <greviansonula@gmail.com>
2023-01-13 05:38:04 +0000

    Update app

--- app.py
+++ app.py
@@ -14,14 +14,13 @@ def index():
   return render_template('templates/index.html')
 
 
-@app.route('/classify', methods=['POST'])
-def classify():
-  text = request.form['text']
-  # preprocess the text
-  text = preprocess_text(text)
-  result = model.predict(text)
-  return render_template('result.html', result=result)
-
+# @app.route('/classify', methods=['POST'])
+# def classify():
+#   text = request.form['text']
+#   # preprocess the text
+#   text = preprocess_text(text)
+#   result = model.predict(text)
+#   return render_template('result.html', result=result)
 
 if __name__ == '__main__':
   app.run(debug=True)
